229380aba68ba9c5a344603972211ea4493da4e3
Ivan <reide740@gmail.com>
2019-01-05 21:23:16 +0300

    Added started page

---
+++ src/views/Game.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="game-page">
+    <canvas id="game-canvas" ref="canvas"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import { Game } from '@/game/game.ts';
+
+@Component
+export default class GamePage extends Vue {
+  private game?: Game;
+
+  public mounted() {
+    const canvas = this.$refs.canvas as HTMLCanvasElement;
+
+    if (canvas != null) {
+      this.game = new Game(canvas);
+
+      this.game.init();
+      this.game.run();
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.game-page {
+  height: 100%;
+  overflow: hidden;
+
+  #game-canvas {
+    width: 100%;
+    height: 100%;
+    touch-action: none;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home-page">
+    <div class="blured header">In memes, we trust...</div>
+    <div class="main">
+      <div class="button" @click="playGame">Play</div>
+    </div>
+    <div class="blured footer"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component
+export default class HomePage extends Vue {
+  public playGame() {
+    this.$router.push({ name: 'game' });
+  }
+}
 </script>
+
+<style lang="scss">
+.home-page {
+  height: 100%;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  font-size: 30pt;
+
+  .button {
+    border-radius: 30px;
+    width: 200px;
+    padding: 10px;
+    background-color: #43A047;
+
+    &:hover {
+      cursor: pointer;
+      background-color: darken(#43A047, 2);
+      box-shadow: 0px 0px 9px 2px #43A047;
+    }
+  }
+
+  .blured {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    height: calc(50% - 15vh);
+    background: url(/img/background.jpg)  no-repeat center center fixed;
+    background-blend-mode: multiply;
+    background-color: rgba(0, 0, 0, 0.4);
+    z-index: -10;
+  }
+
+  .main {
+    height: 30vh;
+    background-color: #252121;
+    box-shadow: 0px 0px 85px 9px rgba(0,0,0,0.75);
+    background-size: cover;
+
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>

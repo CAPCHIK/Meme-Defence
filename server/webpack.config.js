f74a2591d03f1a9cde390e1c7c0ba2dba2a0583f
Maksim <maksalmak@gmail.com>
2019-01-22 17:24:11 +0300

    Done separating projects

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, 'src/index.ts'),
   target: 'node',
-  // externals: [
-  //   /^[a-z\-0-9]+$/
-  // ],
   output: {
     filename: 'server.js',
     path: path.join(__dirname, 'build/'),

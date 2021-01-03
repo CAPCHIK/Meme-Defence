6cfe56143b5f635e2555a2a897de2437b55bfa13
Maksim Makushchenko <maksalmak@gmail.com>
2021-01-03 16:45:28 +0300

    Add react babylonjs render

---
+++ react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css'
+import DefaultPlayground from './DefaultPlayground';
+
+function App() {
+  return (
+      <DefaultPlayground />
+  );
+}
+
+export default App;
